875f47f5e11b1dee651dd1f1bfc0c6ef2078047f
Nicolas Tolosa <ntolosa@MacBook-Pro-de-Nicolas.local>
2024-04-26 22:26:35 -0300

    draft version of matrix render

---
+++ src/components/tetris/tetris.js
@@ -0,0 +1,48 @@
+import './tetris.scss';
+
+const tetrisMatrix = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+];
+
+
+const Tetris = () => {
+    const renderItem = (col) => {
+        if (col === 1) {
+            return <div className='item' />
+        }
+    }
+    const renderMatrix = () => {
+        return tetrisMatrix.map(row => {
+            return <div className="row">
+                {
+                    row.map(col => (<div className="col">
+                        {
+                            renderItem(col)
+                        }
+                    </div>))
+                }
+                </div>  
+            });   
+    };
+    return (
+        <div className='matrix'>
+            { renderMatrix() }
+        </div>
+    )
+}
+
+export default Tetris;
