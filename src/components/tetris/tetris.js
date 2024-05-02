4a4647535e943c3c032ae7ebaf6ff948c67bce10
Nicolas Tolosa <ntolosa@MacBook-Pro-de-Nicolas.local>
2024-05-01 23:48:19 -0300

    draft version for item movement

--- src/components/tetris/tetris.js
+++ src/components/tetris/tetris.js
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react';
 import './tetris.scss';
+
+const ficha = [[1]];
 
 const tetrisMatrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -15,21 +18,58 @@ const tetrisMatrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
+const fichaMatrix = [
+    ...tetrisMatrix
+];
+
+const initialFichaMetadata = {
+    x: 0,
+    y: 5,
+    matrix: fichaMatrix,
+    ficha,
+};
 
 const Tetris = () => {
+    const [fichaMetadata, setFichaMetadata] = useState(initialFichaMetadata);
+
+    const changePosition = (prev) => {
+        let tempX = prev.x;
+        let tempY = prev.y;
+        if (tempX < prev.matrix.length - 1) {
+            let tempMatrix = prev.matrix;
+            tempMatrix[tempX][tempY] = 0;
+            tempX = tempX + 1;
+            tempMatrix[tempX][tempY] = 1;
+
+            return {
+                x: tempX,
+                y: tempY,
+                matrix: tempMatrix,
+                ficha,
+            };
+        }
+        return prev;
+    }
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setFichaMetadata(prev => changePosition(prev))
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+    
     const renderItem = (col) => {
         if (col === 1) {
             return <div className='item' />
         }
     }
     const renderMatrix = () => {
-        return tetrisMatrix.map(row => {
-            return <div className="row">
+        return fichaMetadata.matrix.map((row, i) => {
+            return <div key={i} className="row">
                 {
-                    row.map(col => (<div className="col">
+                    row.map((col, j) => (<div key={j} className="col">
                         {
                             renderItem(col)
                         }
