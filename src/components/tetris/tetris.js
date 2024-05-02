baf80ee4e832e3627c020e985421a6b0c2750b58
ntolosa <njtolosa@gmail.com>
2024-05-02 20:12:39 -0300

    Feature/ficha rendering (#2)
    * draft version for item movement
    
    * adding logic to move ficha
    
    * adding logic to manually move an item
    
    ---------
    
    Co-authored-by: Nicolas Tolosa <ntolosa@MacBook-Pro-de-Nicolas.local>

--- src/components/tetris/tetris.js
+++ src/components/tetris/tetris.js
@@ -1,35 +1,91 @@
+import { useEffect, useState } from 'react';
 import './tetris.scss';
 
-const tetrisMatrix = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+const ficha = [
+    [1, 0, 0],
+    [1, 1, 1]
 ];
 
+const createArray = (count, item) => {
+    const arr = [];
+    for (let i = 0; i < count; i++) {
+        arr.push(structuredClone(item));
+    }
+    return arr;
+}
+
+const row = createArray(10, 0);
+const tetrisMatrix = createArray(20, row);
+
+const fichaMatrix = [
+    ...tetrisMatrix
+];
+
+const initialFichaMetadata = {
+    x: 0,
+    y: 5,
+    matrix: fichaMatrix,
+    ficha,
+};
+
+const moveFicha = (matrix, ficha, x, y) => {
+    const tempMatrix = structuredClone(matrix);
+    for (let i = 0; i < ficha.length; i++) {
+        for (let j = 0; j < ficha[i].length; j++) {
+            tempMatrix[i + x][j + y] = ficha[i][j];
+        }
+    }
+    return tempMatrix;
+}
 
 const Tetris = () => {
+    const [fichaMetadata, setFichaMetadata] = useState(initialFichaMetadata);
+
+    const changePosition = (prev) => {
+        let tempX = prev.x;
+        let tempY = prev.y;
+        if (tempX + ficha.length < prev.matrix.length) {
+            tempX = tempX + 1;
+            const tempMatrix = moveFicha(fichaMatrix, ficha, tempX, tempY);
+
+            return {
+                x: tempX,
+                y: tempY,
+                matrix: tempMatrix,
+                ficha,
+            };
+        }
+        return prev;
+    }
+    const manualMovement = (movement) => {
+        const tempY = fichaMetadata.y + movement;
+        if (tempY >= 0 && tempY + fichaMetadata.ficha[0].length <= fichaMatrix[0].length) {
+            const tempMatrix = moveFicha(fichaMatrix, fichaMetadata.ficha, fichaMetadata.x, tempY);
+            setFichaMetadata({
+                x: fichaMetadata.x,
+                y: tempY,
+                matrix: tempMatrix,
+                ficha
+            });
+        }
+    }
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setFichaMetadata(prev => changePosition(prev))
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+    
     const renderItem = (col) => {
         if (col === 1) {
             return <div className='item' />
         }
     }
     const renderMatrix = () => {
-        return tetrisMatrix.map(row => {
-            return <div className="row">
+        return fichaMetadata.matrix.map((row, i) => {
+            return <div key={i} className="row">
                 {
-                    row.map(col => (<div className="col">
+                    row.map((col, j) => (<div key={j} className="col">
                         {
                             renderItem(col)
                         }
@@ -39,9 +95,16 @@ const Tetris = () => {
             });   
     };
     return (
-        <div className='matrix'>
-            { renderMatrix() }
+        <div>
+            <div className='matrix'>
+                { renderMatrix() }
+            </div>
+            <div>
+                <button onClick={() => manualMovement(-1)}>Left</button>
+                <button onClick={() => manualMovement(1)}>Right</button>
+            </div>
         </div>
+        
     )
 }
 
